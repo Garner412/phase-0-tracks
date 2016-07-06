98c06c6d06c3adb17ab4591a4bba04dd71b12404
Greg Garner <gpgarner10@gmail.com>
2016-07-05 22:21:03 -0400

    Add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
