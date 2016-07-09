847b05b4859c804b93e5cc2f11fcb1aec05ab345
Greg Garner <gpgarner10@gmail.com>
2016-07-09 11:02:47 -0400

    add allergy loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,6 +4,8 @@ puts "How many employees would you like to process?"
 number_employees = gets.to_i
 
 until times_run == number_employees
+	allergy = " "
+
 	puts "What is your name?"
 	name = gets.chomp
 
@@ -16,8 +18,15 @@ until times_run == number_employees
 	puts "Would you like to enroll in the company's health insurance?"
 	insurance = gets.chomp 
 
+	puts "Please list any allergies you may have."
+	until allergy == "done" || allergy == "sunshine"
+		allergy = gets.chomp
+	end
+
 	if name == "Drake Cula" || name == "Tu Fang"
 		puts "Definitely a vampire."
+	elsif allergy == "sunshine"
+		puts "Probably a vampire"		
 	elsif year != "1989" && garlic != "yes" && insurance != "yes"
 		puts "Almost certainly a vampire."
 	elsif year == "1989" && (garlic == "yes" || insurance == "yes")
