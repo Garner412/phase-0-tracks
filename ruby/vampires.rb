21709f5a1ae9364ea93c50eea02aeefd04866ce9
Greg Garner <gpgarner10@gmail.com>
2016-07-09 10:48:49 -0400

    Add loop to process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,31 @@
+times_run = 0
 
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees would you like to process?"
+number_employees = gets.to_i
 
-puts "What year were you born?"
-year = gets.chomp
+until times_run == number_employees
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
+	puts "What year were you born?"
+	year = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic = gets.chomp
 
+	puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp 
+
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire."
+	elsif year != "1989" && garlic != "yes" && insurance != "yes"
+		puts "Almost certainly a vampire."
+	elsif year == "1989" && (garlic == "yes" || insurance == "yes")
+		puts "Probably not a vampire."
+	elsif year != "1989" && (garlic != "yes" || insurance != "yes")
+		puts "Probably a vampire."
+	else
+		puts "Results inconclusive."
+	end
+	times_run += 1
+end
