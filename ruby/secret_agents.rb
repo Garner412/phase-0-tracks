1b567ddf13a6a524f7af81f0f4fdec658f1db432
Greg Garner <gpgarner10@gmail.com>
2016-07-13 21:41:10 -0400

    create encrypt and decrypt methods

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,4 +1,5 @@
 # encrypt - moves one letter forward
+	# determine input length for loop number
 	# search alphabet string for index.
 	# add 1 to index
 	# return that letter(value)
@@ -7,3 +8,40 @@
 	# search alphabet string for index.
 	# subtract 1 to index
 	# return that letter(value)
+
+
+def encrypt(input)
+	new_value = 0
+	new_password = ""
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	times_run = 0
+
+	until times_run == input.length
+		new_value = alphabet.index(input[times_run]) + 1
+		new_password += alphabet[new_value]
+		times_run += 1
+
+	end
+	puts new_password
+end
+
+
+def decrypt(input)
+	new_value = 0
+	new_password = ""
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	times_run = 0
+
+	until times_run == input.length
+		new_value = alphabet.index(input[times_run]) - 1
+		new_password += alphabet[new_value]
+		times_run += 1
+
+	end
+	puts new_password
+end
+
+
+
+encrypt("abc")
+decrypt("bcd")
