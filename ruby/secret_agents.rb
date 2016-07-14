9d16b745fb469b9720974c4fdac983ad3693c53c
Greg Garner <gpgarner10@gmail.com>
2016-07-13 20:42:49 -0400

    pseudocode solution

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,9 @@
+# encrypt - moves one letter forward
+	# search alphabet string for index.
+	# add 1 to index
+	# return that letter(value)
+
+# decrypt - moves one letter backwards
+	# search alphabet string for index.
+	# subtract 1 to index
+	# return that letter(value)
