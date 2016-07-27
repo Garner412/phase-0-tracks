9a9399001f73311cdb00832ebec88b5c4a13518d
Greg Garner <gpgarner10@gmail.com>
2016-07-27 13:17:42 -0400

    add olympic class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,10 @@
 class Puppy
-
+  
+  def initialize
+  #doesn't run if initialize is misspelled
+    puts "Initializing new puppy instance..."
+  end
+  
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -37,3 +42,30 @@ puppy.roll_over
 puppy.dog_years(4)
 
 puppy.favorite_trick("the 360 no handed catch")
+
+
+class Olympics
+  
+  def initialize
+    puts "The Olympics are starting soon!"
+  end
+  
+  def basketball(score)
+    puts "Team USA scores #{score} points."
+  end 
+  
+  def swimming(medals)
+    medals.times {puts "Another gold!"}
+  end
+  
+end
+
+olympics_arr = []
+until olympics_arr.length == 50
+  olympics_arr << Olympics.new
+end
+
+olympics_arr.each do |instance|
+  instance.basketball(50)
+  instance.swimming(1)
+end
