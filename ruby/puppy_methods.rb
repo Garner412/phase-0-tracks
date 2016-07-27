cd990bafa975e44ee2b0b1a862b5c42f49d4e8e2
Greg Garner <gpgarner10@gmail.com>
2016-07-27 12:55:42 -0400

    add methods and driver code to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,36 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
+  def speak(int)
+    int.times {puts "Woof!"}
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def dog_years(age)
+    dog_years = age*7
+    puts dog_years
+  end
+  
+  def favorite_trick(str)
+    puts "My dog's favorite trick is #{str}."
+  end
+  
 end
 
+puppy = Puppy.new
+puppy.fetch("ball")
+
+fido = Puppy.new
+fido.fetch("stick")
+
+puppy.speak(3)
+
+puppy.roll_over
+
+puppy.dog_years(4)
+
+puppy.favorite_trick("the 360 no handed catch")
