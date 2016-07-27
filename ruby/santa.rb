589a87c0ff4e27aef027fe3a61924200ac898118
Greg Garner <gpgarner10@gmail.com>
2016-07-27 14:23:43 -0400

    add random santa generator

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -10,8 +14,57 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie} cookie!"
 	end
+
+	def about
+		puts "Your gender is #{@gender}!"
+		puts "Your ethnicity is #{@ethnicity}!"
+	end
+
 end
 
-santa = Santa.new
+#Testing if new Santa is created
+santa = Santa.new("male", "white")
 santa.speak
 santa.eat_milk_and_cookies("Chocolate Chip")
+santa.about
+
+
+#Inital test after adding attributes
+santa = Santa.new("male", "white")
+santa.about
+
+#Provided example to add new identities
+santas = []
+
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas
+
+#Another initialization to add instances
+santas = []
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
+
+#Creating random Santas
+santas = []
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do
+  santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+  santa.about
+end
+
+
