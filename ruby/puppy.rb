58f4d40412d684ede72a17ecc1e26ba4d06da679
Greg Garner <gpgarner10@gmail.com>
2016-07-27 10:50:38 -0400

    Add class details for puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin
+	
+SPECIES---------
+puppy species
+
+CHARACTERISTICS------
+name:
+color:
+size:
+friendliness:
+energeticness:
+barker:
+
+BEHAVIORS-------
+sit
+laydown
+rollover
+fetch
+	
+=end
