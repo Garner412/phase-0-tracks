e0572c63ae188729e855c5371800499a936ae048
Greg Garner <gpgarner10@gmail.com>
2016-07-27 22:42:57 -0400

    add module code to class instances

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
@@ -15,3 +16,34 @@ end
 
 puts Shout.yell_angrily('Nooooo')
 puts Shout.yelling_happily('woohoo')
+
+=end
+
+module Shout
+	def screeching(words)
+		puts "In a fit of rage, the yelling of #{words} can be heard from the neighboring area code!"
+	end
+
+	def hollaring(words)
+		puts "I told you to #{words}"
+	end
+end
+
+
+class Monkey
+	include Shout
+end
+
+class Mother
+	include Shout
+end
+
+
+
+mother = Mother.new
+mother.screeching("get over here!!!")
+mother.hollaring("do your chores!!")
+
+monkey = Monkey.new
+monkey.screeching("ooo ooo ahhh ahhh!")
+monkey.hollaring("give me that banana!")
