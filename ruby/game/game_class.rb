8b0ddcb481cf4b161ccb2c4025fd30ebeb8391c2
Greg Garner <gpgarner10@gmail.com>
2016-07-30 12:14:50 -0400

    add logic to stop loop if user2 wins before guesses run out

--- ruby/game/game_class.rb
+++ ruby/game/game_class.rb
@@ -1,6 +1,7 @@
 # Game Logic
 
 class Game
+	attr_reader :secret_word, :correct_guess
 
 	def initialize(user1_word)
 		@secret_word = user1_word.split('')
@@ -8,32 +9,47 @@ class Game
 		@wrong_guesses = []
 		@update_spots = []
 		@correct_guess = []
+		user1_word.length.times.each do
+			@correct_guess << "_"
+			@correct_guess
+		end
+
+		p @secret_word.join(' ')
 	end
 
 	def about
 		puts "The secret word is #{@secret_word.join}"
 	end
 
+	def current_status
+		puts @correct_guess.join(' ')
+	end
+
 	def guess_logic(user2_guess)
 		if @wrong_guesses.include?(user2_guess)
 			puts "You've already guessed that letter!"
 			puts "Here is the list of guesses #{@wrong_guesses}"
+			current_status
 		elsif @secret_word.include?(user2_guess)
 			puts "#{user2_guess} is included in the secret word!"
 			right_guess(user2_guess)
+			puts @correct_guess.join(' ')
 		else
 			puts "#{user2_guess} is not included in the secret word!"
 			wrong_guess(user2_guess)
+			current_status
 		end
+		# p @secret_word
+		# p @correct_guess
 	end
 
 	def right_guess(user2_guess)
 		@update_spots = @secret_word.each_index.select {|i| @secret_word[i] == (user2_guess)}
-		p @update_spots
+		# p @update_spots
 		@update_spots.each do |index|
 			@correct_guess[index] = user2_guess
 		end
-		p @correct_guess.join('')
+		@correct_guess.join(' ')
 	end
 
 	def wrong_guess(user2_guess)
@@ -49,6 +65,11 @@ class Game
 	# 	@correct_guess.join(' ')
 	# end
 
+	def winner(secret_word, correct_guess)
+		if @secret_word.join(' ') == @correct_guess.join(' ')
+			puts "You're the winner!!!"
+		end
+	end
 end
 
 
@@ -67,23 +88,29 @@ puts "User 1, please enter the secret word: "
 user1_word = gets.chomp.downcase
 
 game = Game.new(user1_word)
-game.about
+#game.about
 # user1_word.length.times.each do
 # 	correct_guess << "_"
 # 	correct_guess
 # end
-
+# p @correct_guess 
 puts "User 2, the word is #{user1_word.length} letters long."
 puts "Here is the current status of your guessing:"
-# puts game.current_status
+game.current_status
+
 loop do
-	break if guess_count >= user1_word.length
+	break if guess_count >= user1_word.length 
 #game.current_status(user1_word)
+
 puts correct_guess.join(' ')
 puts "User 2, please guess a letter."
 user2_guess = gets.chomp.downcase
-game.guess_logic(user2_guess)
 
+game.guess_logic(user2_guess)
+game.winner(@secret_word, @correct_guess)
+# p game.secret_word
+# p game.correct_guess
+	break if game.secret_word == game.correct_guess
 guess_count += 1
 end
 
