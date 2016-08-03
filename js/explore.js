d0b06a754fe5b1e3354420cc5d6ee329d8c4891f
Greg Garner <gpgarner10@gmail.com>
2016-08-02 21:23:56 -0400

    add pseudocode to reverse string

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+
+// Create a function that acts on a string in the manner that str.reverse does in Ruby
+// To create a reverse of a string we need to take the character at the last index of the inputted string and add it as the first character of a new variable.
+// We then need to work backwards until we reach the 0 index of the string adding each variable to the end of the new variable.
+// We can then return the new variable as our new reversed string.
+
