54eb549268a273f56400b16a08a7dfb4ef013bbe
Greg Garner <gpgarner10@gmail.com>
2016-08-06 11:43:36 -0400

    add object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,8 +1,17 @@
-var colors = ["purple", "blue", "red", "green"];
-var names = ["frank", "herpert", "rudy", "george"];
+var names = ["frank","herpert","rudy","george"];
+var colors = ["purple","blue","red","green"];
 
 colors.push("orange");
 names.push("hal");
 
 console.log(colors);
 console.log(names);
+
+//add code that will use your two arrays to create an object. The keys of your object should be horse names, and the values should be colors.
+
+var horses = {};
+
+for(var i = 0; i < names.length; i++){
+ horses[names[i]] = colors[i];
+}
+console.log(horses);
