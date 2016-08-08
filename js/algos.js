9f75f21f1723d038a2de2d3ff18c84ad7d8a4c22
Greg Garner <gpgarner10@gmail.com>
2016-08-08 01:29:07 -0400

    add pseudocoding for release 1 (longest word)

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,9 @@
 
 // Release 0 - Find the Longest Phrase
-
-
+// Create a function that accepts an array
+// Create a variable that stores the longest word
+// If the next word in the array is longer than the current word, update the variable to the current word. If not, keep the previous word.
+// return the variable containing the longest word.
 
 
 
@@ -23,3 +25,4 @@
 
 
 // Release 2 - Generate Random Test Data
+
