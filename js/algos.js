9e46295cccb487efebe850065dfda48b96acaf39
Greg Garner <gpgarner10@gmail.com>
2016-08-08 09:30:53 -0400

    add release 1 pseudocode

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,6 @@
 
 // Release 0 - Find the Longest Phrase
+
 // Create a function that accepts an array
 // Create a variable that stores the longest word
 // If the next word in the array is longer than the current word, update the variable to the current word. If not, keep the previous word.
@@ -16,15 +17,12 @@ function longest_word(array) {
 }
 
 
-longest_word(["long phrase","longest phrase","longer phrase"])
-longest_word(["z", "zz", "zzz", "zzzz", "zzzzz", "a"])
-longest_word(["five", "three", "two", "four", "one"])
-
-
 // Release 1 - Find a Key-Value Match
 
-
-
+// Create a function that accepts two objects as input
+// Compare the two objects to see if they have a key-value pair in common
+// If there is a match, return true
+// If there is not match, return false
 
 
 
@@ -33,5 +31,26 @@ longest_word(["five", "three", "two", "four", "one"])
 
 
 // Release 2 - Generate Random Test Data
+
+
+
+
+
+
+
+// DRIVE CODE
+
+// Release 0
+longest_word(["long phrase","longest phrase","longer phrase"])
+longest_word(["z", "zz", "zzz", "zzzz", "zzzzz", "a"])
+longest_word(["five", "three", "two", "four", "one"])
+
+
+// Release 1
+
+
+
+
+// Release 2
 
 
