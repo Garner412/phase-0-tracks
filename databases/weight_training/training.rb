4ac90b36c1bf158825c4dc304681eeedd44b95ed
Greg Garner <gpgarner10@gmail.com>
2016-08-13 14:42:51 -0400

    require gems, create db

---
+++ databases/weight_training/training.rb
@@ -0,0 +1,42 @@
+
+# Training Program to calculate weight to use
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+db = SQLite3::Database.new("training.db")
+# db.results_as_hash = true
+
+# # learn about fancy string delimiters
+# create_table_cmd = <<-SQL
+#   CREATE TABLE IF NOT EXISTS kittens(
+#     id INTEGER PRIMARY KEY,
+#     name VARCHAR(255),
+#     age INT
+#   )
+# SQL
+
+# # create a kittens table (if it's not there already)
+# db.execute(create_table_cmd)
+
+# # add a test kitten
+# # db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+
+# # add LOOOOTS of kittens!
+# # so. many. kittens. 
+# #KittenExplosion
+# def create_kitten(db, name, age)
+#   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+# end
+
+# 100.times do
+#   create_kitten(db, Faker::Name.name, 0)
+# end
+
+# # explore ORM by retrieving data
+# # kittens = db.execute("SELECT * FROM kittens")
+# # kittens.each do |kitten|
+# #  puts "#{kitten['name']} is #{kitten['age']}"
+# # end
