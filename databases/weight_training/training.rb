4d014bd43dba075670fa34bd4445cd6468edbc53
Greg Garner <gpgarner10@gmail.com>
2016-08-13 21:36:44 -0400

    update datebase with user input

--- databases/weight_training/training.rb
+++ databases/weight_training/training.rb
@@ -27,26 +27,28 @@ db.execute(create_table_cmd)
 # # add a test user
 # db.execute("INSERT INTO workouts (name, exercise, weight, sets, reps) VALUES ('Tester', 'testercise', 100, 3, 10)")
 
+puts "Please enter your name."
+name = gets.chomp
 
-# # Retrieve Data
+puts "Please enter the exercise you performed."
+exercise = gets.chomp
+
+puts "Please enter the weight you used."
+weight = gets.to_i
+
+puts "Please enter the number of sets."
+sets = gets.to_i
+
+puts "Please enter the number of reps."
+reps = gets.to_i
+
+db.execute("INSERT INTO workouts (name, exercise, weight, sets, reps) VALUES (?, ?, ?, ?, ?)", [name, exercise, weight, sets, reps])
+
+# # explore ORM by retrieving data
+
+
 workout = db.execute("SELECT * FROM workouts")
 workout.each do |workout|
 	puts "#{workout['name']} performed #{workout['sets']} sets of #{workout['reps']} reps with #{workout['weight']} pounds while #{workout['exercise']}ing."
 end
 
-# # add LOOOOTS of kittens!
-# # so. many. kittens. 
-# #KittenExplosion
-# def create_kitten(db, name, age)
-#   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
-# end
-
-# 100.times do
-#   create_kitten(db, Faker::Name.name, 0)
-# end
-
-# # explore ORM by retrieving data
-# # kittens = db.execute("SELECT * FROM kittens")
-# # kittens.each do |kitten|
-# #  puts "#{kitten['name']} is #{kitten['age']}"
-# # end
